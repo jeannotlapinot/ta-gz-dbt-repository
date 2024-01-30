c075df70680923016d49b679bc8e84e1b4b2b3bc
Jeanne Roboh <jeanne.roboh@gmail.com>
2024-01-30 16:51:23 +0000

    created finance model and amended orders operational model

--- models/intermediate/int_orders_operational.sql
+++ models/intermediate/int_orders_operational.sql
@@ -2,10 +2,14 @@
 
 SELECT orders_id,
 date_date,
-ROUND(margin + shipping_fee - logcost - CAST(ship_cost AS FLOAT64),2) AS operational_margin
+ROUND(SUM(margin) + SUM(shipping_fee) - SUM(logcost) - SUM(CAST(ship_cost AS FLOAT64)),2) AS operational_margin,
+SUM(revenue) AS revenue,
+SUM(quantity) AS quantity,
+SUM(margin) AS margin
 FROM {{ref("int_orders_margin")}}
 JOIN
 {{ref("stg_raw__ship")}}
 USING
 (orders_id)
+GROUP BY orders_id, date_date
 ORDER BY orders_id DESC
