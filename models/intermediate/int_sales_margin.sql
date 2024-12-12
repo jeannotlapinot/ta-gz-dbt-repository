ff3bf84b69a1aa27b3c457c28fb19537590bbab9
Jeanne Roboh <jeanne.roboh@gmail.com>
2024-12-12 20:30:19 +0000

    added margin_percent to int_sales-margin model

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,18 +1,15 @@
 -- Margin = Revenue - Purchase_cost
 -- Purchase_cost = Quantity * Purchase_price
 
-WITH tab1 AS (SELECT orders_id
+SELECT orders_id
 , date_date
 , revenue
 , quantity
 , revenue - (quantity * CAST(purchase_price AS FLOAT64)) AS margin
 , quantity * CAST(purchase_price AS FLOAT64) AS purchase_cost
+, {{margin_percent2('revenue', 'quantity * CAST(purchase_price AS FLOAT64)')}} AS margin_percent
 FROM {{ref("stg_raw__sales")}}
 JOIN 
 {{ref("stg_raw__product")}}
 USING
-(products_id))
-
-SELECT *
-, {{margin_percent2('revenue', 'purchase_cost')}}
-FROM tab1
+(products_id)
