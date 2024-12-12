dd7b28288a581a6444fc277fb226c758f6de9aba
Jeanne Roboh <jeanne.roboh@gmail.com>
2024-12-12 20:37:10 +0000

    updated int_campaigns using dbt_utils

--- models/intermediate/int_campaigns.sql
+++ models/intermediate/int_campaigns.sql
@@ -1,15 +1,22 @@
-SELECT *
-FROM
-{{ref("stg_raw__bing")}}
-UNION ALL
-SELECT *
-FROM
-{{ref("stg_raw__criteo")}}
-UNION ALL
-SELECT *
-FROM
-{{ref("stg_raw__facebook")}}
-UNION ALL
-SELECT *
-FROM
-{{ref("stg_raw__adwords")}}
+{{ dbt_utils.union_relations(
+    relations=[ref("stg_raw__bing")
+    ,ref("stg_raw__criteo")
+    , ref("stg_raw__facebook")
+    , ref("stg_raw__adwords") ]
+) }}
+
+-- SELECT *
+-- FROM
+-- {{ref("stg_raw__bing")}}
+-- UNION ALL
+-- SELECT *
+-- FROM
+-- {{ref("stg_raw__criteo")}}
+-- UNION ALL
+-- SELECT *
+-- FROM
+-- {{ref("stg_raw__facebook")}}
+-- UNION ALL
+-- SELECT *
+-- FROM
+-- {{ref("stg_raw__adwords")}}
